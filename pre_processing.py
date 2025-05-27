977cdf93b7d9cb07988df7a47d0087d735a19b06
rise3n <38195409+rise3n@users.noreply.github.com>
2025-05-27 00:46:15 -0700

    data augmentation

--- pre_processing.py
+++ pre_processing.py
@@ -2,7 +2,8 @@ import librosa
 import soundfile as sf 
 import os 
 import pandas as pd
-
+from scipy.signal import fftconvolve
+import numpy as np
 
 type_weights = {
     'shooting': 1.5,
@@ -60,9 +61,8 @@ def assign_priority(row,max_civ,max_death,max_pot):
 
 
 #priority generation, 1 to 5, 1 is highest priority and 5 is lowest 
-def priorityGeneration():
-    file = pd.read_excel(".\data\processed_metadata.xlsx", sheet_name='911_metadata')
-    newfile = file.drop(columns=['link', 'title' ,'date', 'description','file_name'])
+def priorityGeneration(file):
+    newfile = file.drop(columns=['link', 'title' ,'date', 'description'])
     Priority = []
     max_civ = newfile['civilian_initiated'].max()
     max_death = newfile['deaths'].max()
@@ -75,10 +75,82 @@ def priorityGeneration():
     newfile['priority'] = Priority
     print(newfile)
     newfile.to_csv("processed_data.csv", index=True, encoding="utf-8")
+    return newfile
 
+
+#augmentation
+def DataAugment(file, sr):
+    distribution = file['type'].value_counts()
+    #type_augment = ['Fire', 'Missing', 'Robbery', 'Animal', 'Disaster', 'Terror']
+    warning = librosa.load(".\data\\background\\warning.mp3", sr=sr)[0]
+    alarm = librosa.load(".\data\\background\\alarm.mp3", sr=sr)[0]
+    vehicles = librosa.load(".\data\\background\\vehicles.mp3", sr=sr)[0]
+    mix_ratio = 0.2
+    
+    type_augment1 = ['Fire','Missing', 'Robbery','Animal'] 
+    type_augment2 = ['Terror','Disaster'] 
+    steplist = [3,5,7]
+    noiselist = {'warning':warning,'vehicles':vehicles,'alarm':alarm}
+    
+    #add one background noise to type1
+    for type in type_augment1:
+        InstanceList = file.loc[file['type'] == type]
+        for idx, dataframe in InstanceList.iterrows():
+            if dataframe['file_name'] == 'call_506.mp3':
+                continue
+            
+            if not pd.isna(dataframe['file_name']):
+                filename, filetype = dataframe['file_name'].split('.')
+                newinstance = dataframe.copy()
+                recording, sr = librosa.load(".\data\911_recordings\\" + dataframe['file_name'], sr=None)
+                
+                if len(vehicles) < len(recording):
+                    n_tiles = int(np.ceil(len(recording) / len(vehicles)))
+                    vehicles_extended = np.tile(vehicles, n_tiles)[:len(recording)]
+                else:
+                    vehicles_extended = vehicles[:len(recording)]
+
+                #add background
+                noise_recording = recording + mix_ratio * vehicles_extended
+                sf.write(".\data\waveForm\\" + filename + "_vehicle" + ".wav", noise_recording, sr)
+                newinstance['file_name'] = filename + "_" + "vehicles" + ".wav"
+                file.loc[len(file)] = newinstance
+        
+    #augment terror and disaster with multiple background and pitch shift
+    for type in type_augment2:
+        InstanceList = file.loc[file['type'] == type]
+        for idx, dataframe in InstanceList.iterrows():
+            if pd.isna(dataframe['file_name']):
+                continue
+            
+            newinstance = dataframe.copy()
+            filename, filetype = dataframe['file_name'].split('.')
+            recording, sr = librosa.load(".\data\911_recordings\\" + dataframe['file_name'], sr=sr)
+            
+            for key in noiselist:
+                if len(noiselist[key]) < len(recording):
+                    n_tiles = int(np.ceil(len(recording) / len(noiselist[key])))
+                    background_extended = np.tile(noiselist[key], n_tiles)[:len(recording)]
+                else:
+                    background_extended = noiselist[key][:len(recording)]
+                
+                for step in steplist:
+                    recording_processed = librosa.effects.pitch_shift(y=recording, sr=sr, n_steps=step)
+                    recording_processed += mix_ratio * background_extended
+                    sf.write(".\data\waveForm\\" + filename + "_" + str(step) +"shifted" +key+ ".wav", recording_processed, sr)
+                    newinstance['file_name'] = filename + "_" + str(step) +"shifted" +key+ ".wav"
+                    file.loc[len(file)] = newinstance
+
+    file.to_csv('augmented_data.csv', index=False)
+    print("augmentation done")
+
+def main():
+    sr = 16000
+    file = pd.read_excel(".\data\processed_metadata.xlsx", sheet_name='911_metadata')
+    file = priorityGeneration(file)
+    DataAugment(file, sr)
 
 
 if __name__ == "__main__":
-    #loadAllNames()
-    priorityGeneration()
+    main()
     
